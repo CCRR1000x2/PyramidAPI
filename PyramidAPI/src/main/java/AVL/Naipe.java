637445154744cac63f03c0660a1910db46c3ee18
CCRR1000x2 <100001178+CCRR1000x2@users.noreply.github.com>
2022-05-17 00:24:00 -0600

    Finalización clase Naipe

--- PyramidAPI/src/main/java/AVL/Naipe.java
+++ PyramidAPI/src/main/java/AVL/Naipe.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package AVL;
 
 /**
@@ -10,10 +7,10 @@ package AVL;
  */
 public class Naipe {
 
-    int fe, peso, valor;
-    char simbolo;
-    String id;
-    Naipe hijoIzquierdo, hijoDerecho;
+    private int fe, peso, valor;
+    private char simbolo;
+    private String id;
+    private Naipe hijoIzquierdo, hijoDerecho;
 
     public Naipe(String id) {
         this.fe = 0;
@@ -29,47 +26,60 @@ public class Naipe {
 
         char[] ch = valor.toCharArray();
 
-        if (Character.isDigit(ch[0]) && Character.getNumericValue(ch[0]) > 1) {
-            return Character.getNumericValue(ch[0]);
-        } else if (ch.length > 2 && ch[0] == '1' && ch[1] == '0') {
-            return 10;
-        } else if (ch[0] == 'A') {
-            return 1;
-        } else if (ch[0] == 'J') {
-            return 11;
-        } else if (ch[0] == 'Q') {
-            return 12;
-        } else if (ch[0] == 'K') {
-            return 13;
+        if (ch.length == 2) {
+
+            if (Character.isDigit(ch[0]) && Character.getNumericValue(ch[0]) > 1) {
+                return Character.getNumericValue(ch[0]);
+            } else if (ch[0] == 'A') {
+                return 1;
+            } else if (ch[0] == 'J') {
+                return 11;
+            } else if (ch[0] == 'Q') {
+                return 12;
+            } else if (ch[0] == 'K') {
+                return 13;
+            }
+
+        } else if (ch.length == 3) {
+
+            if ((ch[0] == '1') && (ch[1] == '0')) {
+                return 10;
+            }
         }
+
         return 0;
     }
 
     private char setSimbolo(String valor) {
 
         char s = '-';
-        int i = 0;
+        int posSimb = 0;
         char[] ch = valor.toCharArray();
 
         if (ch.length == 2) {
-            i = 1;
-        } else if (ch.length > 2 && ch[0] == '1' && ch[1] == '0') {
-            i = 2;
+            posSimb = 1;
+        } else if ( (ch.length == 3) && (ch[0] == '1') && (ch[1] == '0') ) {
+            posSimb = 2;
         }
         
-        if (i > 0) {
-            switch (ch[i]) {
-                case 't':
-                    s = 't';
+        if (posSimb > 0) {
+            
+            switch (ch[posSimb]) {
+                //case (char) 5:
+                case '♣':    // Trebol   = Alt + 5
+                    s = '♣';
                     break;
-                case 'd':
-                    s = 'd';
+                //case (char) 4:
+                case '♦':    // Diamante = Alt + 4
+                    s = '♦';
                     break;
-                case 'c':
-                    s = 'c';
+                //case (char) 3:
+                case '♥':    // Corazon  = Alt + 3
+                    s = '♥';
                     break;
-                case 'p':
-                    s = 'p';
+                //case (char) 6: 
+                case '♠':    // Pica   = Alt + 6
+                    s = '♠';
                     break;
                 default:
                     break;
@@ -78,27 +88,31 @@ public class Naipe {
         return s;
     }
 
-    private int setValor(char simbolo, int num) {
+    private int setValor(char simbolo, int peso) {
         
         int n = 0;
 
         switch (simbolo) {
-            case 't':
-                n = num;
+            case '♣':
+                n = peso;
                 break;
-            case 'd':
-                n = num + 20;
+            case '♦':
+                n = peso + 20;
                 break;
-            case 'c':
-                n = num + 40;
+            case '♥':
+                n = peso + 40;
                 break;
-            case 'p':
-                n = num + 60;
+            case '♠':
+                n = peso + 60;
                 break;
             default:
                 break;
         }
         return n;
+    }
+
+    public boolean isHoja() {
+        return (this.hijoIzquierdo==null && this.hijoDerecho==null);
     }
 
     public int getFe() {
@@ -133,6 +147,10 @@ public class Naipe {
         this.simbolo = simbolo;
     }
 
+    public boolean isValido() {
+        return ( (this.peso > 0) && (this.simbolo != '-') );
+    }
+
     public String getId() {
         return id;
     }
@@ -158,5 +176,5 @@ public class Naipe {
     }
 
     
-
+    
 }
